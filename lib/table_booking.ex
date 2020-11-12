0fec92bacd1e479e3b4eb9d03ac443a41da8dd39
Kerry Buckley <kerryjbuckley@gmail.com>
2020-11-12 13:27:09 +0000

    Remove generated boilerplate

--- lib/table_booking.ex
+++ lib/table_booking.ex
@@ -1,18 +1,2 @@
 defmodule TableBooking do
-  @moduledoc """
-  Documentation for `TableBooking`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TableBooking.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/table_booking_test.exs
+++ test/table_booking_test.exs
@@ -1,8 +1,3 @@
 defmodule TableBookingTest do
   use ExUnit.Case
-  doctest TableBooking
-
-  test "greets the world" do
-    assert TableBooking.hello() == :world
-  end
 end
