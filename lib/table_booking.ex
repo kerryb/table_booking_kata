ff5e4eb3f39ce9c29bbd4ce82af13625836afde3
Kerry Buckley <kerryjbuckley@gmail.com>
2020-11-12 16:12:30 +0000

    Handle simple single-table booking

--- lib/table_booking.ex
+++ lib/table_booking.ex
@@ -1,12 +1,18 @@
 defmodule TableBooking.Table do
+  @numbers_as_words %{ 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", }
+
   @enforce_keys :capacity
-  defstruct capacity: nil, booked?: false, booked_for: nil
+  defstruct capacity: nil, booked_for: nil
+
+  def output(table) do
+    "table for #{@numbers_as_words[table.booked_for]}"
+  end
 end
 
 defmodule TableBooking do
   alias TableBooking.Table
 
-  @tables Enum.map([8, 6, 4, 4, 3, 3, 2, 2, 2, 2], &%Table{capacity: &1})
+  @tables Enum.map([2, 2, 2, 2, 3, 3, 4, 4, 6, 8], &%Table{capacity: &1})
 
   defstruct requested_bookings: [],
             unaccepted_bookings: [],
@@ -20,13 +26,38 @@ defmodule TableBooking do
     |> format_output()
   end
 
-  defp new(bookings), do: %TableBooking{requested_bookings: Enum.sort(bookings, :desc)}
+  defp new(bookings) do
+    %__MODULE__{requested_bookings: Enum.sort(bookings, :desc)}
+  end
 
-  defp process_requested_bookings(bookings) do
+  defp process_requested_bookings(%{requested_bookings: []} = bookings) do
     bookings
   end
 
-  defp format_output(_bookings) do
-    [[], ""]
+  defp process_requested_bookings(
+         %{
+           requested_bookings: [request | remaining_requests],
+           empty_tables: [table | remaining_tables],
+           booked_tables: booked_tables
+         } = bookings
+       ) do
+    process_requested_bookings(%{
+      bookings
+      | requested_bookings: remaining_requests,
+        empty_tables: remaining_tables,
+        booked_tables: [%{table | booked_for: request} | booked_tables]
+    })
+  end
+
+  defp format_output(bookings) do
+    [output_successful_bookings(bookings), output_failed_bookings(bookings)]
+  end
+
+  defp output_successful_bookings(bookings) do
+    Enum.map(bookings.booked_tables, &Table.output/1)
+  end
+
+  defp output_failed_bookings(_bookings) do
+    ""
   end
 end
--- test/table_booking_test.exs
+++ test/table_booking_test.exs
@@ -6,5 +6,9 @@ defmodule TableBookingTest do
     test "returns an empty list of bookings and an empty unaccepted bookings string when given an empty list" do
       assert reserve([]) == [[], ""]
     end
+
+    test "can accept a simple booking" do
+      assert reserve([2]) == [["table for two"], ""]
+    end
   end
 end
